c75a2643dd7c63eefcf4b9860ba55749e4aed609
Wolodmr <postvezha@gmail.com>
2023-10-16 21:31:16 +0500

    Initial commit

---
+++ age.py
@@ -0,0 +1,3 @@
+print('How old are you?')
+age = int(input())
+print(f'The next year you will be {age+1} old')
---
+++ count.py
@@ -0,0 +1,11 @@
+print('Give some number, please')
+a = float(input())
+print('Give one more number, please')
+b = float(input())
+
+sum = a+b
+difference = a-b
+product = a*b
+quotient = a/b
+
+print(f'Sum: {sum:.2f}, difference: {difference:.2f}, product: {product:.2f}, quotient: {quotient:.2f}')
