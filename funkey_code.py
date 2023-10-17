dde0660a28b8f71c7938b3dc7dd0c51d13aafd5f
Wolodmr <postvezha@gmail.com>
2023-10-17 19:27:34 +0500

    Initial commit

---
+++ funkey_code.py
@@ -0,0 +1,31 @@
+char = ''
+print(f"  {char*4}")
+print(f"{char*2}")
+print(f"{char*2}")
+print(f"{char*2}")
+print(f"  {char*4}")
+
+print()
+
+print(f"  {char*4}")
+print(f"{char*2}    {char*2}")
+print(f"{char*2}    {char*2}")
+print(f"{char*2}    {char*2}")
+print(f"  {char*4}")
+
+print()
+
+print(f"{char*6}")
+print(f"{char*2}    {char*2}")
+print(f"{char*2}    {char*2}")
+print(f"{char*2}    {char*2}")
+print(f"{char*6}")
+
+print()
+
+print(f"{char*6}")
+print(f"{char*2}")
+print(f"{char*4}")
+print(f"{char*2}")
+print(f"{char*6}")
+
